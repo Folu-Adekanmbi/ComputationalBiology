6a9ce706f5ba9a20c75848410e5e07efe47a5bcc
Folu-Adekanmbi <mofolusayo.adekanmbi@gmail.com>
2024-12-25 21:52:04 -0500

    First initial commit
    This is my first commit for my BMI calculator project!

---
+++ BMI Calculator/BMI Calculator.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout << "Welcome to my Beginners BMI Calculator\n";
+    cout << "Test to make sure everything is working properly\n";
+}
+
