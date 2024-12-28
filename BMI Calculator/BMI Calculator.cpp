b9ec64e993b6654d989bd091984326b381369b9e
Folu-Adekanmbi <mofolusayo.adekanmbi@gmail.com>
2024-12-28 12:13:47 -0500

    Second Push
    Just added the function for person to input information for BMI

--- BMI Calculator/BMI Calculator.cpp
+++ BMI Calculator/BMI Calculator.cpp
@@ -7,6 +7,21 @@ int main()
 {
     std::cout << "Hello World!\n";
     cout << "Welcome to my Beginners BMI Calculator\n";
-    cout << "Test to make sure everything is working properly\n";
+
+    int age;
+    cout << "How old are you?\n";
+    cin >> age; 
+    string sex;
+    cout << "What is your sex?\n";
+    cin >> sex;
+    double height;
+    cout << "What's your height in centimetres? \n";
+    cin >> height;
+    double weight;
+    cout << "What is your weight in kg\n";
+    cin >> weight;
+
+
+
 }
 
