e2178d66d3fe6381a6f2304588fdf5f1e2271069
Folu-Adekanmbi <mofolusayo.adekanmbi@gmail.com>
2025-01-07 16:41:41 -0500

    Push 3
    Used for loop to identify each character in input string and use that information to get the complementary base sequence however still needs improving.

--- DNA Sequencer Project/DNA_Sequencer_Project.py
+++ DNA Sequencer Project/DNA_Sequencer_Project.py
@@ -1,3 +1,14 @@
 print("Hello world!")
 name = input("What is your name?\n")
 print("Nice to meet you " + name)
+sequence = input ("Input your DNA sequence :\n")
+
+for base in sequence:
+    if base == "A":
+        print ("T",end = "")
+    elif base == "T":
+        print ("A" ,end = "")
+    elif base == "C":
+        print ("G",end = "")
+    elif base == "G":
+        print ("C",end = "")
